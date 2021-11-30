1d198da7904217b125f2ecb5b594898473dea826
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-30 16:11:33 +0100

    1-Module1-DeLaCreationDuServeurALaMiseEnPlaceDeLaStructure-Etape2:LaPageDAccueil(1/2)

--- application/serveur.js
+++ application/serveur.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.set('view engine','ejs');
 
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
 app.listen(9090, function() {
     console.log('Mon serveur écoute usr le port 9090 !');
 })
