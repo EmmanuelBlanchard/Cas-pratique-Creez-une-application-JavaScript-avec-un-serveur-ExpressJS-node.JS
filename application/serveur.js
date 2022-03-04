70021055f8d6e3b3f866724ffc6355b798bdd2aa
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-03-04 18:38:17 +0100

    1-Module1-DeLaCreationDuServeurALaMiseEnPlaceDeLaStructure-Etape3:CreerUneStructureDeSite-Correction

--- application/serveur.js
+++ application/serveur.js
@@ -1,12 +1,18 @@
+// Charger les éléments dont nous avons besoin
 const express = require('express');
 const app = express();
 
+// Définir le moteur d'affichage à ejs
 app.set('view engine','ejs');
 
-app.get('/', function(req, res) {
+// Utiliser res.render pour charger un fichier de vue ejs
+
+// Page d'accueil
+app.get('/',function(req, res) {
     res.render('index', {pseudo : "toto",age : 23});
 });
 
 app.listen(9090, function() {
     console.log('Mon serveur écoute sur le port 9090 !');
 });
+
