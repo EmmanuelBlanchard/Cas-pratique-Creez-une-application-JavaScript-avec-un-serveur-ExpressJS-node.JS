b84b1600b561decd4b9323374af0243a7835e0f2
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-03-04 22:06:08 +0100

    2-Module2-LaRealisationDesPagesLieesAuxPersonnages-Etape7:ModificationDeArmeDUnPersonnage(1/2)

--- application/serveur.js
+++ application/serveur.js
@@ -2,12 +2,15 @@
 const personnages = require('./personnages.json');
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
 
 // Définir le moteur d'affichage à ejs
 app.set('view engine','ejs');
 
 // Lorsqu'un client fait la demande d'un fichier, il doit aller le chercher dans le dossier public
 app.use(express.static('public'));
+// Utilisation de bodyParser
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // Utiliser res.render pour charger un fichier de vue ejs
 
@@ -25,6 +28,10 @@ app.get('/personnages', function(req, res) {
 app.get('/personnage', function(req, res) {
     var idPersonnage = req.query.perso;
     res.render('personnage', {perso : personnages[idPersonnage]});
+});
+
+app.post('/personnage', function(req, res) {
+    console.log(req.body);
 });
 
 app.listen(9090, function() {
